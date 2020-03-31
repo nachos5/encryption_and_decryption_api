0fc9973204e875a7a722236f3eb9a5ea2ab2badf
nachos5 <gnorland93@gmail.com>
2020-03-31 13:12:25 +0000

    x

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
 from app import api
 
-try:
-    app.config.from_envvar("CONFIG_OBJECT")
-except Exception as e:
-    print(e)
+if os.environ.get("IS_PRODUCTION"):
+    print("PRODUCTION")
+    app.config.from_object("config.ProductionConfig")
+else:
     app.config.from_object("config.DevelopmentConfig")
 
 print(app.config["SECRET_KEY"])
