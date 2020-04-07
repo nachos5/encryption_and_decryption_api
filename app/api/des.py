34ca9e5f3b9bc03ef734f24bb338967c7dbad8a1
slowpoke <hjaltigeir@gmail.com>
2020-04-07 15:20:39 +0000

    des, des3 done tested, error handling, different modes and iv

--- app/api/des.py
+++ app/api/des.py
@@ -1,5 +1,5 @@
-import base64
 import binascii
+from base64 import b64encode, b64decode
 
 from flask import jsonify
 from flask_restx import reqparse, abort, Api, Resource
@@ -12,8 +12,13 @@ from . import api
 # Single DES
 # 64 bit (8 bytes or 8 ASCII charactes) key
 
+allowed_modes = ['ECB', 'CBC', 'CFB', 'OFB']
+allowed_iv_modes = ['base64', 'utf8', 'utf-8']
+
 des_encrypt_parser = reqparse.RequestParser()
 des_encrypt_parser.add_argument("key", required=True, type=str)
+des_encrypt_parser.add_argument("mode", required=True, type=str)
+des_encrypt_parser.add_argument("iv", type=str)
 des_encrypt_parser.add_argument("message", required=True, type=str)
 
 class DESEncrypt(Resource):
@@ -21,19 +26,59 @@ class DESEncrypt(Resource):
   @api.doc(response={200: "Success", 400: "Validation Error"})
   def post(self):
     args = des_encrypt_parser.parse_args()
+    key = args["key"]
+    mode = args["mode"].upper()
+    message = args["message"]
+    iv = args["iv"]
+
+    # Error handling
+    if len(key.encode("utf-8")) != DES.block_size:
+      abort(400, "Invalid key, must be of length 8")
+    else:
+      key = str.encode(key)
+    if mode not in allowed_modes:
+      abort(400, f'Invalid mode, must be {allowed_modes}')
+    if iv is not None:
+      if mode == 'ECB':
+        abort(400, "Mode 'ECB' doesn't use an Initialization vector")
+      elif len(iv.encode("utf-8")) != 8:
+        abort(400, "Initialization vector must be of length 8")
+      else:
+        iv = str.encode(iv)
+    if message is None:
+      abort(400, "No message to encrypt")
+    else:
+      message = str.encode(message)
+
     try:
-      key = str.encode(args["key"])
+    # Electronic Code Book
+      if mode == 'ECB':
+        cipher = DES.new(key, DES.MODE_ECB)
+        encrypted = cipher.encrypt(pad(message, DES.block_size))
+        cipher_iv = ''
+      # Cipher-Block Chaining
+      elif mode == 'CBC':
+        cipher = DES.new(key, DES.MODE_CBC, iv)
+        encrypted = cipher.encrypt(pad(message, DES.block_size))
+        cipher_iv = b64encode(cipher.iv).decode("utf-8")
+      # Cipher FeedBack
+      elif mode == 'CFB':
+        cipher = DES.new(key, DES.MODE_CFB, iv)
+        encrypted = cipher.encrypt(message)
+        cipher_iv = b64encode(cipher.iv).decode("utf-8")
+      # Output FeedBack
+      elif mode == 'OFB':
+        cipher = DES.new(key, DES.MODE_OFB, iv)
+        encrypted = cipher.encrypt(message)
+        cipher_iv = b64encode(cipher.iv).decode("utf-8")
     except Exception as e:
       print(e)
-      abort(400, "Invalid public key format.")
-    message = args["message"]
+      abort(500, "Unknown error")
 
-    encryptor = DES.new(key, DES.MODE_ECB)
-    encrypted = encryptor.encrypt(pad(str.encode(message), 16))
-
-    # we return a base64 representation
+    # Return base64
     data = {
-      'encrypted_message': base64.b64encode(encrypted)#.decode("ascii")
+      'iv': cipher_iv,
+      'encrypted_message': b64encode(encrypted).decode("utf-8")
     }
 
     return jsonify(data)
@@ -41,24 +86,79 @@ class DESEncrypt(Resource):
 
 des_decrypt_parser = reqparse.RequestParser()
 des_decrypt_parser.add_argument("key", required=True, type=str)
+des_decrypt_parser.add_argument("mode", required=True, type=str)
+des_decrypt_parser.add_argument("iv_mode", type=str)
+des_decrypt_parser.add_argument("iv", type=str)
 des_decrypt_parser.add_argument("encrypted_message", required=True, type=str, help="Encrypted message in base64 format.")
 
 class DESDecrypt(Resource):
-  @api.expect(des_encrypt_parser)
+  @api.expect(des_decrypt_parser)
   @api.doc(response={200: "Success", 400: "Validation Error"})
   def post(self):
-    args = des_encrypt_parser.parse_args()
+    args = des_decrypt_parser.parse_args()
+    key = args["key"]
+    mode = args["mode"].upper()
+    encrypted_message = args["encrypted_message"]
+    iv_mode = args["iv_mode"]
+    iv = args["iv"]
+
+    # Error handling
+    if len(key.encode("utf-8")) != DES.block_size:
+      abort(400, "Invalid key, must be of length 8")
+    else:
+      key = str.encode(key)
+    if mode not in allowed_modes:
+      abort(400, f'Invalid mode, must be {allowed_modes}')
+    if iv is not None:
+      if mode == 'ECB':
+        abort(400, "Mode 'ECB' doesn't use an Initialization vector")
+
+      if iv_mode not in allowed_iv_modes:
+        abort(400, f'Invalid mode, must be {allowed_iv_modes}')
+      
+      # Accept both utf-8 and base64
+      if iv_mode == 'base64':
+        try:
+          iv = b64decode(iv)
+        except Exception as e:
+          print(e)
+          abort(400, "Invalid base64 string")
+      elif len(iv.encode("utf-8")) != 8:
+        abort(400, "Initialization vector must be of length 8")
+      else:
+        iv = str.encode(iv)
+
+    if encrypted_message is not None:
+      try:
+        encrypted_message = b64decode(encrypted_message)
+      except binascii.Error as e:
+        print(e)
+        abort(400, "Message has to be encoded in base64")
+    else:
+      abort(400, "No message to decrypt")
+
     try:
-      key = str.encode(args["key"])
+      # Electronic Code Book
+      if mode == 'ECB':
+        cipher = DES.new(key, DES.MODE_ECB)
+        decrypted = unpad(cipher.decrypt(encrypted_message), DES.block_size)
+      # Cipher-Block Chaining
+      elif mode == 'CBC':
+        cipher = DES.new(key, DES.MODE_CBC, iv)
+        decrypted = unpad(cipher.decrypt(encrypted_message), DES.block_size)
+      # Cipher FeedBack
+      elif mode == 'CFB':
+        cipher = DES.new(key, DES.MODE_CFB, iv)
+        decrypted = cipher.decrypt(encrypted_message)
+      # Output FeedBack
+      elif mode == 'OFB':
+        cipher = DES.new(key, DES.MODE_OFB, iv)
+        decrypted = cipher.decrypt(encrypted_message)
     except Exception as e:
       print(e)
-      abort(400, "Invalid public key format.")
-    encrypted_message = base64.b64decode(args["encrypted_message"])
+      abort(500, "Unknown error")
 
-    decryptor = DES.new(key, DES.MODE_ECB)
-    decrypted = unpad(decryptor.decrypt(encrypted_message), 16)
-
-    # we return a base64 representation
+    # Return base64
     data = {
       'decrypted_message': decrypted.decode()
     }
